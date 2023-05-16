fce3423001c8e6adaa25f650161f78286a62e249
woodemai <n.savchenkoo73@gmail.com>
2023-05-16 19:46:33 +0300

    set base window and config

--- src/ru/vsu/cs/sem2task4_27/Window.java
+++ src/ru/vsu/cs/sem2task4_27/Window.java
@@ -1,13 +1,8 @@
 package ru.vsu.cs.sem2task4_27;
 
-import ru.vsu.cs.sem2task4_27.utils.IOUtils;
-import ru.vsu.cs.sem2task4_27.utils.JTableUtils;
-
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.awt.*;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Window extends JFrame {
     private JButton buttonInputDataArray;
@@ -23,43 +18,14 @@ public class Window extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(panel);
         setVisible(true);
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension dimension = toolkit.getScreenSize();
-        int width = dimension.width / 2;
-        int height = dimension.height / 2;
-        setBounds(width / 2, height / 2, width, height);
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setCurrentDirectory(new File("."));
-        FileFilter filter = new FileNameExtensionFilter("Text files", "txt");
-        fileChooser.addChoosableFileFilter(filter);
-        JTableUtils.initJTableForArray(tableDataArray, 40, false, true, false, true);
-        JTableUtils.initJTableForArray(tableOrderArray, 40, false, true, false, true);
-        JTableUtils.initJTableForArray(tableOutput, 40, false, true, false, true);
         buttonInputDataArray.addActionListener(e -> {
-            try {
-                if (fileChooser.showOpenDialog(panel) == JFileChooser.APPROVE_OPTION) {
-                    String[] array = IOUtils.getStringArrayFromFile(fileChooser.getSelectedFile().getPath());
-                    JTableUtils.writeArrayToJTable(tableDataArray, array);
-                }
-            } catch (Exception exception) {
-                throw new RuntimeException(exception);
-            }
+
         });
         buttonInputOrderArray.addActionListener(e -> {
-            try {
-                if (fileChooser.showOpenDialog(panel) == JFileChooser.APPROVE_OPTION) {
-                    int[] array = IOUtils.getIntArrayFromFile(fileChooser.getSelectedFile().getPath());
-                    JTableUtils.writeArrayToJTable(tableOrderArray, array);
-                }
-            } catch (Exception exception) {
-                throw new RuntimeException(exception);
-            }
+
         });
         buttonExecute.addActionListener(e -> {
-            String[] sortData = JTableUtils.readStringMatrixFromJTable(tableDataArray)[0];
-            int[] sortOrder = JTableUtils.readIntArrayFromJTable(tableOrderArray);
-            Sort.sort(sortData, sortOrder);
-            JTableUtils.writeArrayToJTable(tableOutput, sortData);
+
         });
     }
 }
